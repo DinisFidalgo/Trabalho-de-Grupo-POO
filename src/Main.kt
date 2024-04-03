35de3c57ceb299533ad9937ebf9899ed83c2cf07
Dinis Fidalgo <dinisfidalgo@ua.pt>
2024-04-03 09:56:41 +0100

    First Commit inicial class

--- src/Main.kt
+++ src/Main.kt
@@ -5,9 +5,9 @@ fun main() {
     val expenseManager =  ExpenseManager()
 
 
-    expenseManager.newTransaction("01/01/2023",TransactionType.EXPENSE,"Almoçarada",774.99,"Comi que nem uma besta!")
-    expenseManager.newTransaction("01/01/2023",TransactionType.EXPENSE,"Sobre A Mesa",5.0,"Pensei que fosse mais caro")
-    expenseManager.newTransaction("02/01/2023",TransactionType.INCOME,"Reembolso da Almoçarada",774.99, "Chamei o gregório e devolveram-me a guita")
+    expenseManager.newTransaction("01/01/2023",TransactionType.EXPENSE,"Almoçarada",74.99,"Comi num restaurante fantástico!")
+    expenseManager.newTransaction("01/01/2023",TransactionType.EXPENSE,"Sobremesa",5.0,"Pudim de ovos")
+    expenseManager.newTransaction("02/01/2023",TransactionType.INCOME,"Abono Familiar",774.99, "O estado pagou-me o abono")
 
     val allTransactions = expenseManager.getTransactions()
 
