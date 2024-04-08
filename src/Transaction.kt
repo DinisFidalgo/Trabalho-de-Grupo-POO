9a0a17e06547ce623ae1abebeee8484b3e74e04c
DinisFidalgo <165783896+DinisFidalgo@users.noreply.github.com>
2024-04-08 15:34:53 +0100

    Add notes to transaction

--- src/Transaction.kt
+++ src/Transaction.kt
@@ -1,10 +1,11 @@
 
 enum class TransactionType {
-    INCOME, EXPENSE
+    INCOME, EXPENSE   // Representam os ganhos e os gastos, respetivamente
 }
-
+// Class que vai representar a transação
 data class Transaction(
-    val date: String,
+    // Atributos da transação, a data, o tipo, a categoria, a quantidade e uma nota opcional
+    val date: String,  
     val type: TransactionType,
     val category: String,
     val amount: Double,
