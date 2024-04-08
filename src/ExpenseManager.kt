9a6c52eb943b21da39f501732d4d0ab63fcd7ace
DinisFidalgo <165783896+DinisFidalgo@users.noreply.github.com>
2024-04-08 15:29:23 +0100

    Add notes to Code
    Some notes have been added to help understand the functionality of the code

--- src/ExpenseManager.kt
+++ src/ExpenseManager.kt
@@ -6,11 +6,12 @@ import TransactionType
 class ExpenseManager {
 
     private val transactions : MutableList<Transaction> = mutableListOf()
-
+    //Função para que permite adicionar uma nova transação
     fun newTransaction(date: String, type: TransactionType, category: String, amount: Double, note: String? = null) {
+    // Criar e adicionar a transação à lista
         transactions.add(Transaction(date, type, category, amount, note))
     }
-
+    // Função para obter todas as transações efectuadas
     fun getTransactions(): List<Transaction> {
         return transactions.toList()
     }
