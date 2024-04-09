d242f575cd47fd9025fdeb31f5ad7cd7a173b08d
DinisFidalgo <165783896+DinisFidalgo@users.noreply.github.com>
2024-04-09 17:49:19 +0100

    New code test

--- src/Main.kt
+++ src/Main.kt
@@ -5,7 +5,7 @@ fun main() {
     val expenseManager =  ExpenseManager()
 
 
-    expenseManager.newTransaction("01/01/2023",TransactionType.EXPENSE,"Almoçarada",74.99,"Comi num restaurante fantástico!")
+    expenseManager.newTransaction("01/01/2023",TransactionType.EXPENSE,"Almoço",74.99,"Comi num restaurante fantástico!")
     expenseManager.newTransaction("01/01/2023",TransactionType.EXPENSE,"Sobremesa",5.0,"Pudim de ovos")
     expenseManager.newTransaction("02/01/2023",TransactionType.INCOME,"Abono Familiar",774.99, "O estado pagou-me o abono")
 
