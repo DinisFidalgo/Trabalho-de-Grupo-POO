1a3c5324e717f375795641a92caa2a2079f685fc
Dinis-Fidalgo-120841 <dinisfidalgo@ua.pt>
2024-04-09 19:18:59 +0100

    Loader update

--- src/FinancialGoal.kt
+++ src/FinancialGoal.kt
@@ -3,19 +3,23 @@ class FinancialGoal {
 
         private val goals: MutableList<FinancialGoal> = mutableListOf()
 
-        init {
-                File("C:\\Users\\Laptop\\Desktop\\GitHub grupo\\Trabalho-de-Grupo-POO\\src\\finalcialGoals.csv").forEachLine { line ->
-                        val parts = line.split(",").map { it.trim() }
-                        val description = parts[0]
-                        val value = parts[1]
-                        val startDate = parts[2]
-                        val endDate = parts[3]
+        fun goalsLoader(){
 
-                }
+                        File("src/financialGoals.csv").forEachLine { line ->
+                                val parts = line.split(",").map { it.trim() }
+                                val description = parts[0]
+                                val value = parts[1]
+                                val startDate = parts[2]
+                                val endDate = parts[3]
+
+                        }
+
 
         }
 
+        fun newFinancialGoal (){}
 
 
 
 }
+
