848344f7b8a279f80138c5b7dce7dc72f0ea466d
Dinis-Fidalgo-120841 <dinisfidalgo@ua.pt>
2024-04-13 14:05:15 +0100

    FinancialGoal Loader update

--- src/FinancialGoal.kt
+++ src/FinancialGoal.kt
@@ -1,25 +1,27 @@
 import java.io.File
-class FinancialGoal {
+data class FinancialGoal (
+        val description: String,
+        val value: Double,
+        val startDate: String,
+        val endDate: String
+)
 
+
+
+class FinancialGoalLoader {
         private val goals: MutableList<FinancialGoal> = mutableListOf()
 
-        fun goalsLoader(){
+        fun goalsLoader(file: String){
+                File(file).forEachLine { line ->
+                        val parts = line.split(",").map { it.trim() }
+                        val description = parts[0]
+                        val value = parts[1].toDouble() ?: 0.0
+                        val startDate = parts[2]
+                        val endDate = parts[3]
 
-                        File("src/financialGoals.csv").forEachLine { line ->
-                                val parts = line.split(",").map { it.trim() }
-                                val description = parts[0]
-                                val value = parts[1]
-                                val startDate = parts[2]
-                                val endDate = parts[3]
-
-                        }
-
-
+                        val goal = FinancialGoal (description, value, startDate, endDate)
+                }
         }
-
-        fun newFinancialGoal (){}
-
-
 
 }
 
