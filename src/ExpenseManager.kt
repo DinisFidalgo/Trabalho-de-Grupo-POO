4fe94fc74eaa959c08250b207f293366763c3de0
Dinis-Fidalgo-120841 <dinisfidalgo@ua.pt>
2024-04-20 15:27:40 +0100

    Added exception handling to avoid errors

--- src/ExpenseManager.kt
+++ src/ExpenseManager.kt
@@ -7,7 +7,12 @@ class ExpenseManager {
     //Função para que permite adicionar uma nova transação
     fun newTransaction(date: String, type: TransactionType, category: String, amount: Double, note: String? = null) {
     // Criar e adicionar a transação à lista
-        transactions.add(Transaction(date, type, category, amount, note))
+        // Tratamento de exceção no caso de ocorrer um erro
+        try {
+            transactions.add(Transaction(date, type, category, amount, note))
+        } catch (e: Exception){
+            println("Transação não adicionada \n Error: ${e.message} ")
+        }
     }
     // Função para obter todas as transações efectuadas
     fun getTransactions(): List<Transaction> {
