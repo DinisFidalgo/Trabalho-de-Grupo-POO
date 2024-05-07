cb88020d73044505fc6736d5640dee83095098f8
Dinis-Fidalgo-120841 <dinis.fidalgo.27@gmail.com>
2024-05-07 12:39:38 +0100

    Main update, for a cleaner menu

--- src/Main.kt
+++ src/Main.kt
@@ -16,7 +16,7 @@ fun main() {
 
     var option = readln().toInt()
 
-//    while (option != 3){
+    while (option != 3){
 
         when (option) {
             1 -> {
@@ -30,25 +30,88 @@ fun main() {
                     println("---Menu---")
                     println("Saldo: ${list[2]}")
                     println("Numero de conta: ${list[3]}")
-                    println("1.Criar Objetivo")
-                    println("1.Verificar Objetivo")
+                    println("1.Transações")
+                    println("2.Objetivos")
                     println("3.Sair")
                     var option3 = readln().toInt()
                     when (option3){
                         1->{
-                            println("Nome do objetivo:")
-                            val nome = readLine().toString()
-                            println("Custo:")
-                            val custo = readln().toInt()
-                            println("Dias para completar o objetivo:")
-                            val dias = readln().toInt()
-                            FinancialGoalLoader().createGoal(nome,custo,dias,list[3].toInt())
+                            println("---Transações---")
+                            println("1 - Adicionar Transação ")
+                            println("2 - Consultar Transações ")
+                            println("3 - Sair")
+
+                            var option = readln().toInt()
+                            when (option) {
+                                1 -> {
+                                    println("Nova Transação")
+                                    println("Data (DD/MM/AAAA): ")
+                                    val date = readln().toString()
+
+                                    println("Tipo (INCOME/EXPENSE): ")
+                                    val transactionType = TransactionType.valueOf(readln().toString().toUpperCase())
+
+                                    println("Categoria: ")
+                                    val transactionCategory = readln().toString()
+
+                                    println("Valor: ")
+                                    val transactionAmount = readln().toDouble()
+
+                                    println("Nota Adicional (Opcional): ")
+                                    val note = readln().toString()
+
+                                    expenseManager.newTransaction(date,transactionType,transactionCategory,transactionAmount,note)
+
+                                    println("A sua transação foi adicionada com sucesso!")
+                                }
+                                2-> {
+                                    println("Estas são todas as suas transações:")
+
+                                    val transactions = expenseManager.getTransactions()
+                                    if (transactions.isEmpty()){
+                                        println("Ainda não efectuou nenhuma transação")
+
+                                } else {
+                                    transactions.forEachIndexed { index, transaction ->
+                                        println("${index + 1}. $transaction")
+                                    }
+                                } }
+                                3 -> println("Aplicação encerrada")
+                                else -> println("A opção introduzida é inválida! Insira uma nova opção.")
+
+                            }
+
                         }
                         2->{
-                            println(FinancialGoalLoader().getGoals(list[3].toInt()).forEach{println(it[0].dropLast(2))})
+                            println("---Objetivos---")
+                            println("1 - Adicionar Objetivo")
+                            println("2 - Consultar Objetivos")
+
+                            var option = readln().toInt()
+
+                            when(option) {
+                                1->{
+                                    println("Descrição do Objetivo: ")
+                                    val nome = readLine().toString()
+                                    println("Custo:")
+                                    val custo = readln().toInt()
+                                    println("Dias para completar o objetivo:")
+                                    val dias = readln().toInt()
+                                    FinancialGoalLoader().createGoal(nome,custo,dias,list[3].toInt())
+                                }
+
+                                2-> {
+                                    // println(FinancialGoalLoader().getGoals(list[3].toInt()).forEach{println(it[0].dropLast(2))})
+                                     val financialGoals = FinancialGoalLoader().getGoals(list[3].toInt())
+                                    for (goals in financialGoals) {
+                                        println(goals[0].dropLast(2))
+                                    }
+                                }
+                            }
+
                         }
                         3->{"A sair"}
-                        else->{println("erro")}
+                        else->{println("Opção Inválida!")}
                     }
 
                 }
@@ -70,8 +133,7 @@ fun main() {
                 }
             }
         }
-
-//    }
+    }
 //
 //    var option2 = 0
 //
